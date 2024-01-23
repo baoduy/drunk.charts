e8538943d5f727955826da556487f0b24fc314fe
Steven Hoang <drunkcoding@outlook.com>
2024-01-23 11:48:02 +0800

    update

--- drunk-app/verify.sh
+++ drunk-app/verify.sh
@@ -1,4 +1,4 @@
 rm -rf ./_test
 helm template test ./ --values ./values.yaml --output-dir ./_test
-helm package ./ --app-version "1.0.0"
+helm package ./
 helm install -f ./values.yaml hello-world ./ --dry-run
