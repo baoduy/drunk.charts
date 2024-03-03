30f41758a342d9c560819715381320888b4bea81
Steven Hoang <hoangbaoduy@gmail.com>
2024-03-03 10:31:25 +0800

    added cronJobs

--- drunk-app/verify.sh
+++ drunk-app/verify.sh
@@ -1,5 +1,5 @@
-rm -rf ./_test
-helm template test ./ --values ./values.yaml --output-dir ./_test
+rm -rf ../_test
+helm template test ./ --values ./values.test.yaml --output-dir ../_test --debug
 helm package ./
 #helm install -f ./values.yaml hello-world ./ --dry-run
 helm repo index ./
