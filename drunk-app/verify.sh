25e93af30dfc94da5c4677152f74df2a6e2eda60
Steven Hoang <hoangbaoduy@gmail.com>
2024-03-03 10:42:42 +0800

    add jobs

--- drunk-app/verify.sh
+++ drunk-app/verify.sh
@@ -1,5 +1,5 @@
 rm -rf ../_test
 helm template test ./ --values ./values.test.yaml --output-dir ../_test --debug
 helm package ./
-#helm install -f ./values.yaml hello-world ./ --dry-run
+helm install -f ./values.test.yaml test ./ --debug --dry-run
 helm repo index ./
